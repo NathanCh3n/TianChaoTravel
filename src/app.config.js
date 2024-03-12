81824bb79d8130455abba844c52f04aaf14c1021
nathan.chen <503400835@qq.com>
2024-03-12 16:18:42 +0800

    全局配置-配置tabBar

--- src/app.config.js
+++ src/app.config.js
@@ -1,11 +1,32 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/order/order'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
+    navigationBarTitleText: 'Travel',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    color: '#7F8389',
+    selectedColor: '#5495e6',
+    borderStyle: 'black',
+    backgroundColor: '#ffffff',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页',
+        iconPath: 'assets/images/index-unselected.png',
+        selectedIconPath: 'assets/images/index-selected.png'
+      },
+      {
+        pagePath: 'pages/order/order',
+        text: '订单',
+        iconPath: 'assets/images/order-unselected.png',
+        selectedIconPath: 'assets/images/order-selected.png'
+      }
+    ]
   }
 })

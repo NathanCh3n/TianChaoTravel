4a49e646c5f807a87712dfb61ad01618c440aaeb
nathan.chen <503400835@qq.com>
2024-03-15 15:51:07 +0800

    服务端搭建

---
+++ server/mysql.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql2')
+
+const options = {
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '153193',
+    database: 'faraway',
+}
+
+// 创建数据库连
+const connection = mysql.createConnection(options);
+connection.connect(err => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('数据库连接成功！')
+})
+
+// 执行mysql查询命令
+/**
+ * @param
+ */
+const sqlQuery = (strsql) => {
+    return new Promise((resolve, reject) => {
+        connection.query(strsql, (err, res) => {
+            if(err) {
+                reject(err)
+            } else {
+                resolve(res)
+            }
+        })
+    })
+}
+
+module.exports = sqlQuery
